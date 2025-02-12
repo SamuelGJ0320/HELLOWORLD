b67cc537fd6f1e944ef17d2d0650f507d01ec16d
Samuel Gutierrez Jaramillo <sgutierr28@eafit.edu.co>
2025-02-11 22:41:49 -0500

    Initial commit

---
+++ HELLOWORLD_project/models.py
@@ -0,0 +1,6 @@
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
