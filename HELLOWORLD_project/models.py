6e098d4479acb067e96c2fec747ddbbc6fbcdccb
Samuel Gutierrez Jaramillo <sgutierr28@eafit.edu.co>
2025-02-12 22:06:44 -0500

    Tutorial 2

--- HELLOWORLD_project/models.py
+++
@@ -1,6 +0,0 @@
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.IntegerField()
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
--- pages/models.py
+++ pages/models.py
@@ -6,5 +6,6 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.name
+class Comment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    description = models.TextField()
