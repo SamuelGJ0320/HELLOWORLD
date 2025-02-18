968dbb1e9cdce5d72ac1508815c8b35f2aa34e9d
Samuel Gutierrez Jaramillo <sgutierr28@eafit.edu.co>
2025-02-17 23:15:20 -0500

    Taller 3 parte A

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ProductShowView
+from .views import *
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path("", HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
-    path('products/', ProductShowView.as_view(), name='products'),  # Aquí defines /products/
+    path('products/', ProductIndexView.as_view(), name='index'),
+    path('products/create', ProductCreateView.as_view(), name='form' ),
+    path('products/<str:id>/', ProductShowView.as_view(), name='show'),
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'), 
 ]
