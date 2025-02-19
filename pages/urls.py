ec234ebb2cfecb6288912bab6b4f382421fa8cb8
Samuel Gutierrez Jaramillo <sgutierr28@eafit.edu.co>
2025-02-19 16:17:52 -0500

    Tutorial 3 completo

--- pages/urls.py
+++ pages/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('products/<str:id>/', ProductShowView.as_view(), name='show'),
     path('cart/', CartView.as_view(), name='cart_index'),
     path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
-    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'), 
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+    path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenodi_index'),
+    path('imagenotdi/save', ImageViewNoDI.as_view(), name='imagenodi_save'),
 ]
